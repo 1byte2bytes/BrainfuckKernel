e7f0d28f8da1ca1225c4ca06c6a52c2c191a444f
Sydney Erickson <sydneyerickson@icloud.com>
2018-03-27 14:42:33 -0700

    Add comments (DOES NOT FIX ANY BUGS)

--- brainfuck.c
+++ brainfuck.c
@@ -25,40 +25,54 @@ void main() {
 
 	for(int i = 0; i < strlen(program); i++) {
 		if(program[i] == '>') {
+			// Go up one cell
 			cellpos = getNewCellPos(1);
 
 		} else if(program[i] == '<') {
+			// Go down one cell
 			cellpos = getNewCellPos(-1);
 
 		} else if(program[i] == '+') {
+			// Increase number in cell
 			cells[cellpos]++;
 
 		} else if(program[i] == '-') {
+			// Decrease number in cell
 			cells[cellpos]--;
 
 		} else if(program[i] == '.') {
+			// Print number in cell
 			printf("%c", cells[cellpos]);
 
 		} else if(program[i] == ',') {
+			// Get input character
 			cells[cellpos] = getchar();
 
 		} else if(program[i] == '[' && cells[cellpos] == 0) {
+			// If the beginning of a loop and the cell content is zero
+			// Set some useful values
 			bool foundMatch = false;
 			uint32_t newpos = i;
 			uint32_t innerloops = 0;
 			while(foundMatch != true) {
+				// Loop until a proper match is found
 				newpos++;
 				if(program[newpos] == '[') {
+					// Detect a nested loop
 					innerloops++;
 				}
 				if(program[newpos] == ']') {
+					// Detect loop end bracket
 					if(innerloops == 0) {
+						// If no inner loops, we've hit the bracket we need
 						foundMatch = true;
 						//printf("[%i --> %i]\n", newpos, i);
 						i = newpos;
 					}else if(innerloops > 0) {
+						// Detected the end of an inner loop, keep going
 						innerloops--;
 					}else{
+						// Our loop counter went below 0 somehow
 						printf("Invalid innerloops value\n");
 						return;
 					}
@@ -66,22 +80,30 @@ void main() {
 			}
 
 		} else if(program[i] == ']' && cells[cellpos] != 0) {
+			// If the end of a loop and the cell content is not zero, keep loop going
+			// Set some useful values
 			bool foundMatch = false;
 			uint32_t newpos = i;
 			uint32_t innerloops = 0;
 			while(foundMatch != true) {
+				// Loop until matching bracket is found
 				newpos--;
 				if(program[newpos] == ']') {
+					// We've detected an inner loop and must keep track
 					innerloops++;
 				}
 				if(program[newpos] == '[') {
+					// Detected beginning bracket for loop
 					if(innerloops == 0) {
+						// If no more inner loops, we've hit the one we need
 						foundMatch = true;
 						//printf("[%i <-- %i]\n", newpos, i);
 						i = newpos;
 					}else if(innerloops > 0) {
+						// Detected beginning of an inside loop, keep going
 						innerloops--;
 					}else{
+						// Loop counter somehow hit negatives
 						printf("Invalid innerloops value\n");
 						return;
 					}
